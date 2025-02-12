a78f09840df38d01f595590efd69598e7793ec5e
Srinu-mylapalli <srinivasmylapali@gmail.com>
2025-02-12 22:46:52 +0530

    Update armstrong Number.c

--- armstrong Number.c
+++ armstrong Number.c
@@ -2,8 +2,22 @@
 #include<conio.h>
 int main (){
 int r,num,temp,sum=0;
+clrscr();
 printf("Enter a number: ");
 scanf("%d",num);
 temp=num;
 while(temp>0){
 r=temp%10;
+sum +=(r*r*r);
+temp=temp/10;
+}
+//checking condition 
+if(sum==num){
+  printf("%d is Armstrong number",num);
+}
+else{
+  printf("%d is not armstrong number",num);
+}
+getch();
+return 0;
+}
