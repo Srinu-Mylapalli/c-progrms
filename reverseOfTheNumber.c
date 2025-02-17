5595e6c612f5a1c6b02f804b65a7b3fb7dad9a3f
Srinu-mylapalli <srinivasmylapali@gmail.com>
2025-02-17 18:50:44 +0530

    reverseOfTheNumber.c

---
+++ reverseOfTheNumber.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+  int l1,l2,r,dup,sum=0;
+printf("Enter the two limits: ");
+scanf("%d%d",&l1,&l2);
+while(l1<=l2)
+{
+dup = l1;
+sum=0;
+while(dup>0)
+{
+r=dup%10;
+sum=(sum*10)+r;
+dup=dup/10;
+}
+printf("%d",sum);
+l1++;
+}
+}
