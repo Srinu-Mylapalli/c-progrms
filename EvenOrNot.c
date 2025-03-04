897612addf2bfba84a28e3d69c114e1ee0f81aa5
Srinu-Mylapalli <srinivasmylapali@gmail.com>
2025-03-04 20:06:37 +0530

    EvenOrNot.c

---
+++ EvenOrNot.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,rem;
+  clrscr();
+printf("Enter a number: ");
+rem=a%2;
+if (rem==0)
+{
+printf("%d is even\n",a);
+}
+else
+{
+  printf("%d is odd",a)
+}
+  getch();
+}
